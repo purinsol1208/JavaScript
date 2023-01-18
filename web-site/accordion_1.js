c1b428f6581b88c0edaf4a65be8b6f6e4d5c4337
purinsol1208 <70867051+purinsol1208@users.noreply.github.com>
2023-01-18 21:34:15 +0900

    active

--- web-site/accordion_1.js
+++ web-site/accordion_1.js
@@ -5,7 +5,16 @@ console.log(panelFaqContainer);
 //반복문 순회 하면서 해당 FAQ 제목 클릭 시 콜백 처리
 for(let i = 0; i < panelFaqContainer.length; i++) {
     panelFaqContainer[i].addEventListener('click', function() {
-        console.log(i + 'click!');
+        //console.log(i + 'click!');
+
+        //FAQ 본문 선택
+        let panelFaqAnswer = document.querySelectorAll('.panel-faq-answer');
+        //console.log(panelFaqAnswer[i]);
+
+        /*FAQ 제목 클릭 시 -> 본문이 보이게끔 -> active 클래스 추가
+        this.classList.add('active'); -> 현재 this는 panelForContainer이지만
+                                        panel-faq-answer 클래스에 active를 추가 해야함 */
+        panelFaqAnswer[i].classList.add('active');
     });
 }
 
